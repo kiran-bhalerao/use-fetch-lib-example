dd9263f95ff7d19368c8d015383100eef87beb41
kiranbhalerao1234 <kiran@vrlabtech.com>
2020-03-31 22:02:10 +0530

    Cache example

--- src/Another.tsx
+++ src/Another.tsx
@@ -1,4 +1,7 @@
-import React, { useEffect } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/accessible-emoji */
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React, { useEffect, useState } from "react";
 import { useFetch } from "use-fetch-lib";
 
 const mockTodos = {
@@ -20,37 +23,77 @@ interface IPostData extends IPostTodo {
   id: number;
 }
 
-export const Another: React.FC = () => {
-  const [todo, { isFulfilled: todoFulfilled }, recall] = useFetch<IGetTodos>({
+const SubComponent: React.FC<any> = ({ name }) => {
+  const [todo, { isFulfilled, isMocked, isCached }, recall] = useFetch<
+    IGetTodos
+  >({
     url: "/todos/1",
     method: "get",
-    shouldDispatch: true,
-    cancelable: true
+    mockData: {
+      id: 2,
+      title: "Mock title",
+      userId: 223,
+      completed: false
+    },
+    beforeServiceCall: () => {
+      console.log(`Called ${name}`);
+    },
+    cache: true
   });
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      recall();
-    }, 2000);
+    console.log(name, isFulfilled, isMocked, isCached);
+  }, [isFulfilled, isMocked, isCached]);
 
-    return () => {
-      clearInterval(interval);
-    };
-  }, [recall]);
+  const [clicks, updateClicks] = useState(1);
 
   return (
     <>
-      <br />
-      <h3>useFetch `get` call</h3>
+      <div>
+        <span style={{ fontWeight: "bold" }}>Hits: </span>({clicks})
+      </div>
       <div>
         <span style={{ fontWeight: "bold" }}>Data: </span>{" "}
         {todo?.title && todo?.title}
       </div>
       <div>
-        <span style={{ fontWeight: "bold" }}>State: </span>
-        {todoFulfilled ? "Todos loaded.. 💯" : "Loading Todos... 🏃"}
+        <span style={{ fontWeight: "bold" }}>status: </span>
+        {isFulfilled ? "Todos loaded.. 💯" : "Loading Todos... 🏃"}
+      </div>
+      <div>
+        <span style={{ fontWeight: "bold" }}>from: </span>
+        {isMocked
+          ? "Mock"
+          : isCached
+          ? "Cache"
+          : isFulfilled
+          ? "Network"
+          : "Pending"}
       </div>
       <br />
+      <button
+        onClick={() => {
+          updateClicks(pre => pre + 1);
+          recall();
+        }}
+      >
+        Hit <span role="img">✋</span>
+      </button>
     </>
+  );
+};
+
+export const Another: React.FC = () => {
+  return (
+    <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+      <div>
+        <h1>Component</h1>
+        <SubComponent name="Component" />
+      </div>
+      <div>
+        <h1>Another Component</h1>
+        <SubComponent name="Another Component" />
+      </div>
+    </div>
   );
 };
